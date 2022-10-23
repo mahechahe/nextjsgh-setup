efe1e8c8c2c219c4a50928811d74ab331004332e
Estiven Mahecha henao <mahechahe@gmail.com>
2022-10-23 13:01:36 -0500

    Hello world updated

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          NextJS CI/CD con Github Actions
+          Hello world
         </h1>
 
         <p className={styles.description}>
