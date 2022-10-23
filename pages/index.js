122040514634f90eae76b1753aee970c083f82af
Estiven Mahecha henao <mahechahe@gmail.com>
2022-10-23 11:41:27 -0500

    Setup project

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          NextJS CI/CD con Github Actions
         </h1>
 
         <p className={styles.description}>
